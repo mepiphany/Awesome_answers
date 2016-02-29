e1f12f1b854af6658ec8f2545626a756a33610c2
Michael Lee <mike.epiphany@gmail.com>
2016-02-29 15:52:21 -0800

    AJAXIFY likes, and favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,24 +4,27 @@ class FavoritesController < ApplicationController
     @question = current_user.favored_questions
   end
 
-
   def create
-    q = Question.find params[:question_id]
-    favor = Favorite.new(question: q, user: current_user)
-    favor.save
-    redirect_to q, notice: "Added to your favorite"
+    @question = Question.find params[:question_id]
+    favor = Favorite.new(question: @question, user: current_user)
+    respond_to do |format|
+      if favor.save
+        format.html { redirect_to @question, notice: "Added to your favorite" }
+        format.js { render :successful_favor }
+      else
+        format.html { redirect_to @question, alert: "like added to your favorite" }
+        format.js { render :unsuccessful_favor }
+      end
+    end
   end
 
   def destroy
+    @question = Question.find params[:question_id]
     favor = current_user.favorites.find params[:id] # This will ensure that only the current_user can change the favor
-    q = Question.find params[:question_id]
     favor.destroy
-    redirect_to q, notice: "removed"
-
+      respond_to do |format|
+      format.html { redirect_to @question, notice: "removed" }
+      format.js { render }
+   end
   end
-
-
-
-
-
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,20 +2,26 @@ class LikesController < ApplicationController
   before_action :authenticate_user
 
   def create
-    q = Question.find params[:question_id]
-    like = Like.new(question: q, user: current_user)
-    if like.save
-      redirect_to q, notice: "Liked!"
-    else
-      redirect_to q, notice: "not Liked!"
-    end
-  end
+    @question = Question.find params[:question_id]
+    like = Like.new(question: @question, user: current_user)
+    respond_to do |format|
+       if like.save
+         format.html { redirect_to @question, notice: "Liked!" }
+         format.js   { render :successfully_liked }
+       else
+         format.html { redirect_to @question, alert: "Not Liked!" }
+         format.js   { render :unsuccessfully_liked };
+       end
+     end
+   end
 
   def destroy
-    like = current_user.like.find params[:id]
-    q    = Question.find params[:question_id]
+    like = current_user.likes.find params[:id]
+    @question    = Question.find params[:question_id]
     like.destroy
-    redirect_to question_path(q), notice: "Un-liked!"
-  end
-
+    respond_to do |format|
+       format.html { redirect_to question_path(@question), notice: "Un-liked!" }
+       format.js   { render }
+     end
+   end
 end
